3b641de4e34fb4a0594a461254f1454973b6b7aa
Alex Murray <murray.alex@gmail.com>
2023-02-10 09:55:45 +1030

    Bump version to 0.5
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- apparmor-mode.el
+++ apparmor-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Alex Murray <murray.alex@gmail.com>
 ;; Maintainer: Alex Murray <murray.alex@gmail.com>
 ;; URL: https://github.com/alexmurray/apparmor-mode
-;; Version: 0.4
+;; Version: 0.5
 ;; Package-Requires: ((emacs "26.1"))
 
 ;; This file is not part of GNU Emacs.
